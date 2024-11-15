51530fd6409d8a973c7079bfa9ce8f0033d23717
ashubhankhar <157944297+ashubhankhar@users.noreply.github.com>
2024-11-15 09:16:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -55,7 +55,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/waspros")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/dramafilez")
         self.LOGGER(__name__).info(f""" \n\n       
 (っ◔◡◔)っ ♥   @Dramafilez   ♥
 ░╚════╝░░╚════╝░╚═════╝░╚════╝░
