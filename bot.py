cba5a49477a77319e58f71e183a36a1f558ee9a6
ashubhankhar <157944297+ashubhankhar@users.noreply.github.com>
2024-11-15 09:11:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,7 +57,7 @@ class Bot(Client):
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/waspros")
         self.LOGGER(__name__).info(f""" \n\n       
-(っ◔◡◔)っ ♥ @Dramafilez ♥
+(っ◔◡◔)っ ♥   @Dramafilez   ♥
 ░╚════╝░░╚════╝░╚═════╝░╚════╝░
                                           """)
         self.username = usr_bot_me.username
