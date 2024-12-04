53dc84c904b18c1569c52cdf8d82f23e6d05504b
ashubhankhar <157944297+ashubhankhar@users.noreply.github.com>
2024-12-05 00:39:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "publicearn.in")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "9cf7ecb5b4707087b91bf1262b76d97e5417ffa0")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
-IS_VERIFY = os.environ.get("IS_VERIFY", "True")
+IS_VERIFY = os.environ.get("IS_VERIFY", "False")
 TUT_VID = os.environ.get("TUT_VID","https://t.me/korean_dramas_bot_Dramafilez/9")
 
 #force sub channel id, if you want enable force sub
